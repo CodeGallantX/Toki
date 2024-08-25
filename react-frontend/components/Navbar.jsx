359310d1bfb53302c321b3f7a4004a35f0530ea3
CodeGallantX <johnayobami77@proton.me>
2024-08-25 12:41:20 +0100

    added navbar component

---
+++ react-frontend/components/Navbar.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react';
+
+// const Navbar = () => {
+//   return (
+//     <div>
+//       <nav>
+
+//       </nav>
+//     </div>
+//   )
+// }
+
+// export default Navbar
